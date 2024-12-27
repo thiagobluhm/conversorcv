f5acb5e10a16d9e12014eefa1be8c11bbcefd9d8
Thiago Bluhm <thiagobluhm@gmail.com>
2024-12-27 14:49:04 -0300

    comitando correcao engine

--- engine.py
+++ engine.py
@@ -159,7 +159,7 @@ def process_text(texto):
             "educacao": [],
             "certificacoes": []
         }
-        return validate_json(dados_json, estrutura_padrao)
+        return validate_json(resultado.content, estrutura_padrao)
 
     except Exception as e:
         print("Erro ao processar texto com a API OpenAI:", e)
